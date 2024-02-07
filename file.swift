c72c7d850d86baf10970f3ea3a4048ddb93b9653
Jann Fiedler <jannfiedler@icloud.com>
2024-02-07 09:59:44 +0100

    Initial Commit: Added support for custom Linting
    This adds
    - a swiftlint.yml file ( config ),
    - a file.swift ( for rules testing ),
    - a implemented accessibility rule

--- file.swift
+++ file.swift
@@ -1,1 +1,15 @@
-let forbidden : Int = 1
+// let forbidden: Int = 1
+
+// class Foo {}
+// class Bar {}
+
+// protocol Foo {}
+// enum Bar {}
+
+struct MyView: View {
+    var body: some View {
+        Image("my-image")
+            .resizable(true)
+            .frame(width: 48, height: 48)
+    }
+}
